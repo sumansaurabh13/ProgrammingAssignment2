d95fdd19e0daa0d356c2a5f51c69a08c5ec65b50
sumansaurabh13 <sumansaurabh13.iitkgp@gmail.com>
2016-02-22 05:07:35 +0530

    Week 3 Assignment; GitHub user: sumansaurabh13
    Week 3 Assignment; GitHub user: sumansaurabh13 by Suman Saurabh

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,72 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Week 3 Assignment; GitHub user: sumansaurabh13
+## makeCacheMatrix returns a list of functions which is used by cacheSolve 
+## to compute the inverse of the matrix returned by makeCacheMatrix above
+makeCacheMatrix <- function(x = matrix()) { # storing the cached value
+  invmat <- NULL # initializing invmat to NULL
+  
+  # creating the matrix in the working environment
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  
+  # getting the value of the matrix
+  get <- function() x
+  
+  # inverting the matrix and storing in cache
+  setinv <- function(inverse) invmat <<- inverse
+  
+  # getting the inverted matrix from cache
+  getinv <- function() invmat 
+  
+  # returning the created functions to the working environment
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## attempting to get the inverse of the matrix stored in cache
+  invmat <- x$getInverse()
+  
+  # returning inverted matrix from cache if it exists
+  # else creating the matrix in working environment
+  if (!is.null(invmat )) {
+    message("getting cached data")
+    
+    # displaying matrix in console
+    return(invmat)
+  }
+  
+  # creating matrix since it does not exist
+  matrix <- x$get()
+  
+  # making sure matrix is square and invertible
+  tryCatch( {
+    # set and return inverse of matrix
+    invmat <- solve(matrix, ...)
+  },
+  error = function(e) {
+    message("Error:")
+    message(e)
+    
+    return(NA)
+  },
+  warning = function(e) {
+    message("Warning:")
+    message(e)
+    
+    return(NA)
+  },
+  finally = {
+    # set inverted matrix in cache
+    x$setMatrix(invmat)
+  } )
+  
+  # displaying matrix in console
+  return (invmat)
 }
